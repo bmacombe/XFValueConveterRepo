7265b9abd302fe8e1ea2cf9a61833b25dc56f9a9
Brian Macomber <brian@agspectrum.com>
2018-11-16 11:07:32 -0600

    Repo

---
+++ XFValueConverterRepo/XFValueConverterRepo/DummyValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XFValueConverterRepo
+{
+    public class DummyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
